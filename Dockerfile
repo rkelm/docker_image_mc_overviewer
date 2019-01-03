5666672161483d94e866d7ba60a92cf15b6e4998
vagrant <git@kelm.net>
2019-01-03 21:17:23 +0000

    Files grabbed from original docker bakery; removed private configuration settings

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM ubuntu:18.10
+
+ENV APP_NAME Overviewer for Minecraft
+ENV APP_DIR INSTALL
+ENV MC_VERSION 1.13
+ENV DATA = $(date -Idate)
+
+RUN apt-get update && echo $DATE
+RUN echo "deb https://overviewer.org/debian ./" >> /etc/apt/sources.list
+RUN apt-get -y install apt-transport-https > /dev/null
+RUN apt-get -y install wget > /dev/null
+RUN wget -nv -O - https://overviewer.org/debian/overviewer.gpg.asc | apt-key add -
+RUN apt-get update && echo $DATE
+RUN apt-get -y install minecraft-overviewer > /dev/null
+RUN wget -nv https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar -P ~/.minecraft/versions/${MC_VERSION}/
+
+RUN echo -e ' ************************************************** \n' \
+  "Docker Image to run app ${APP_NAME} ${MC_VERSION}. \n" \
+  '\n' \
+  'Usage: \n' \
+  "   Run overviewer:	docker run -v <host-world-dir>:/root/world \\ \n" \
+  "                             -v <host-config-&-texture-dir>:/root/config \\ \n" \
+  "                             -v <host-render-output-dir>:/root/render_output \\ \n" \
+  "                             <image_name> overviewer.py --config=/root/config/<config file> /root/render_output\n" \
+  "   Simple run without config: \"
+  "   	  	  	docker run -v <host-world-dir>:/root/world \\ \n" \
+  "                             -v <host-render-output-dir>:/root/render_output \\ \n" \
+  "                             <image_name> overviewer.py /root/world /root/render_output \n" \  
+  "   Configure overviewer: \n" \
+  "			Put configuration file(s) in volume mounted to /root/config. \n" \
+  "			Output will be rendered in /root/render_output. \n" \
+'**************************************************' > /image_info.txt
+
+RUN mkdir /root/world
+RUN mkdir /root/config
+RUN mkdir /root/render_output
+
+# Path to map files. Path to config file. Path to texture file.
+VOLUME ["/root/world","/root/config","/root/render_output"]
+
+CMD ["/bin/cat", "/image_info.txt"]
---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+    echo "usage: $(basename $0) <mc_version>"
+    exit 1
+fi
+
+errchk() {
+    if [ "$1" != "0" ] ; then
+	echo "$2"
+	echo "Exiting."
+	exit 1
+    fi
+}
+
+# ***** Configuration *****
+# Assign configuration values here or set environment variables.
+local_repo_path="$BAKERY_LOCAL_REPO_PATH"
+remote_repo_path="$BAKERY_REMOTE_REPO_PATH"
+repo_name="overviewer_minecraft"
+
+# Default server properties may be changed below.
+# Some options may be set directly in the Dockerfile.
+if [ -z "$local_repo_path" ] || [ -z "$remote_repo_path" ] ; then
+    errchk 1 'Configuration variables in script not set. Assign values in script or set corresponding environment variables.'
+fi
+
+MC_VERSION="$1"
+image_tag="$MC_VERSION"
+
+# The project directory is the folder containing this script.
+project_dir=$( dirname "$0" )
+project_dir=$( ( cd "$project_dir" && pwd ) )
+if [ -z "$project_dir" ] ; then
+    errck 1 "Error: Could not determine project_dir."
+fi
+echo "Project directory is ${project_dir}."
+
+if [ -n "$image_tag" ] ; then
+    local_repo_tag="${local_repo_path}/${repo_name}:${image_tag}"
+    remote_repo_tag="${remote_repo_path}/${repo_name}:${image_tag}"    
+else
+    local_repo_tag="${local_repo_path}:${repo_name}"
+    remote_repo_tag="${remote_repo_path}:${repo_name}"
+fi
+
+# Build.
+echo "Building $local_repo_tag"
+APP_VERSION="${APP_VERSION}" docker build "${project_dir}" -t "${local_repo_tag}"
+errchk $? 'Docker build failed.'
+
+# Get image id.
+image_id=$(docker images -q "${local_repo_tag}")
+
+test -n $image_id
+errchk $? 'Could not retrieve docker image id.'
+echo "Image id is ${image_id}."
+
+# Tag for Upload to aws repo.
+docker tag "${image_id}" "${remote_repo_tag}"
+errchk $? "Failed re-tagging image ${image_id}".
+
+# Upload.
+echo "Execute the following commands to upload the image to remote aws repository."
+echo '   $(aws ecr get-login --no-include-email --region eu-central-1)'
+echo "   docker push ${remote_repo_tag}"
